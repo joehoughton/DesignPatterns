4428444a3ad5eaaca9443214d57bf769f303b647
Joe Houghton <joe.houghton@houghtonjoe.com>
2020-10-16 11:54:58 +0200

    Prototype

---
+++ DesignPatterns.CopyThroughSerialization/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace DesignPatterns.CopyThroughSerialization
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// To use the BinaryFormatter, classes must be [Serializable]
+        /// </summary>
+        public static T DeepCopy<T>(this T self)
+        {
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T)copy;
+        }
+
+        /// <summary>
+        /// XMLFormatter allows us to get away with using [Serializable]
+        /// However, classes must have a parameterless constructor
+        /// </summary>
+        public static T DeepCopyXml<T>(this T self)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var s = new XmlSerializer(typeof(T));
+                s.Serialize(ms, self);
+                ms.Position = 0;
+                return (T)s.Deserialize(ms);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    [Serializable]
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+
+            var jane = john.DeepCopy();
+            jane.Names[0] = "Jane";
+            jane.Address.HouseNumber = 833;
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns.ExplicitDeepCopyInterface/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/// <summary>
+/// IClonable is bad because it only performs shallow copy
+/// This approach aims to solve the issue of deep copy but can be tedious for deep structures
+/// </summary>
+namespace DesignPatterns.ExplicitDeepCopyInterface
+{
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+
+    public class Person : IPrototype<Person>
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(Names, Address.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    public class Address : IPrototype<Address>
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address DeepCopy()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+
+            var jane = john.DeepCopy();
+            jane.Names[0] = "Jane";
+            jane.Address.HouseNumber = 833;
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+
+            Console.ReadLine();
+        }
+    }
+}
